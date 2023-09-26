3c4e49394935ac523e5785709533e85c2350dd58
Charles Talbot <charles.talbot@bjss.com>
2023-09-26 14:23:38 +0100

    split main into 2 more functions

--- workset/goQuiz/quiz.go
+++ workset/goQuiz/quiz.go
@@ -12,18 +12,17 @@ import (
 // store score
 
 func main() {
-	questions := []string{"What is 1+1?", "What is 2+2?", "What is 3+3", "What is 7x8", "What is 9X7?", "how many bits is in 10001011?"}
+	questions := []string{"What is 1+1?", "What is 2+2?", "What is 3+3", "What is 7x8", "What is 9X7?", "what number does 10001011 represent?"}
 	answers := []int{2, 4, 6, 56, 63, 139}
 
 	total := 0
-	var userGuess int
 	qNum := 1
 
 	for i := 1; i <= len(questions); {
 		question, indexNumber := getQuestion(questions)
-
 		fmt.Println("Question", qNum, question)
-		fmt.Scanln(&userGuess)
+
+		userGuess := getUserGuess()
 
 		result := checkAnswer(userGuess, indexNumber, answers)
 		fmt.Println(result)
@@ -31,8 +30,7 @@ func main() {
 		total += changeScore(total, result)
 		fmt.Println("Your score is", total)
 
-		questions = append(questions[:indexNumber], questions[indexNumber+1:]...)
-		answers = append(answers[:indexNumber], answers[indexNumber+1:]...)
+		questions, answers = removeQuestionAndAnswer(questions, answers, indexNumber)
 		qNum++
 	}
 	fmt.Println("That is the end of the quiz! Your score is", total)
@@ -46,6 +44,13 @@ func getQuestion(questions []string) (string, int) {
 	question := questions[indexNumber]
 
 	return question, indexNumber
+}
+
+func getUserGuess() int {
+	var userGuess int
+	fmt.Print("Your answer: ")
+	fmt.Scanln(&userGuess)
+	return userGuess
 }
 
 func checkAnswer(userGuess, indexNumber int, answers []int) string {
@@ -62,4 +67,10 @@ func changeScore(total int, result string) int {
 	} else {
 		return -50
 	}
+}
+
+func removeQuestionAndAnswer(questions []string, answers []int, indexNumber int) ([]string, []int) {
+	questions = append(questions[:indexNumber], questions[indexNumber+1:]...)
+	answers = append(answers[:indexNumber], answers[indexNumber+1:]...)
+	return questions, answers
 }
